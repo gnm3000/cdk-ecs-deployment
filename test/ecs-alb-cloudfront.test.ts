9643ae22df966dfe93f4c7f72ad15ad249d1d743
German N Montenegro <gnm3000@gmail.com>
2022-10-19 12:59:32 -0300

    Initial commit

---
+++ test/ecs-alb-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsAlbCloudfront from '../lib/ecs-alb-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-alb-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsAlbCloudfront.EcsAlbCloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
